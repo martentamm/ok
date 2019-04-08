90d5ad5311f95ea1baeea5a4a94f0d81618eacc1
Marten Tamm <marten.tamm@gmail.com>
2019-04-08 20:07:36 +0300

    Updates

---
+++ precache-manifest.635eaf7efa0e26cf98ae221ca928c787.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "645b35f8aab38c4be357",
+    "url": "/ok/static/js/runtime~main.e3fbccdb.js"
+  },
+  {
+    "revision": "af96018d5c6e7302444a",
+    "url": "/ok/static/js/main.d822c167.chunk.js"
+  },
+  {
+    "revision": "671c31f6c9ffb59cba71",
+    "url": "/ok/static/js/2.616410f5.chunk.js"
+  },
+  {
+    "revision": "af96018d5c6e7302444a",
+    "url": "/ok/static/css/main.bdb2f4aa.chunk.css"
+  },
+  {
+    "revision": "0fa165b7046c6e0ed01a6647b927b3ea",
+    "url": "/ok/index.html"
+  }
+];
